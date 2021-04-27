9e059c7dab7beb95843b7067d1cb0467517b98f2
Andrey <sinko02@inbox.ru>
2021-04-27 13:19:29 +0300

    Поменял деление на умножение

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     int a, b;
     printf("Enter two integer numbers:");
     scanf("%d %d", a, b);
-    printf("a+b=%d\na-b=%d", a+b, a-b);
-    printf("a/b=%d", a/b);
+    printf("a-b=%d\na+b=%d", a-b, a+b);
+    printf("a*b=%d", a*b);
     return 0;
 }
